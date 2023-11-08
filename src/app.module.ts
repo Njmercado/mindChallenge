20130e4d38ad00c21fc054d2317f262653788c2d
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-08 10:15:55 -0500

    feat(app.module): added team module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GroupModule } from './group/group.module';
 import { AccountModule } from './account/account.module';
+import { TeamModule } from './team/team.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://localhost/administrator-db-api'),
-    GroupModule,
     AccountModule,
+    TeamModule,
   ],
   controllers: [AppController],
   providers: [AppService],
