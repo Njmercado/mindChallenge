46cc6f01b71c5f9e8542ac477edddef2e9474c5a
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-08 10:10:22 -0500

    feat(account.service): finished by the moment account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { Account } from './schemas/acccount.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { ResponseAccountDto } from './dto/response-account.dto';
 
 @Injectable()
 export class AccountService {
-  create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+
+  constructor(@InjectModel(Account.name) private readonly accountModel: Model<Account>) {}
+
+  async create(createAccountDto: CreateAccountDto): Promise<ResponseAccountDto> {
+    try {
+      const saveResponse = await this.accountModel.create(createAccountDto);
+      if(saveResponse._id) {
+        return this.findOne(saveResponse._id.toString());
+      }
+    } catch(error) {
+      console.log(error.message);
+      return null;
+    }
   }
 
-  findAll() {
-    return `This action returns all account`;
+  async findAll(): Promise<ResponseAccountDto[]> {
+    return this.accountModel.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
+  async findOne(id: string): Promise<ResponseAccountDto> {
+    return this.accountModel.findById(id);
   }
 
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
+  async update(id: string, updateAccountDto: UpdateAccountDto): Promise<ResponseAccountDto[]> {
+    return this.accountModel.findByIdAndUpdate(id, updateAccountDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
+  async remove(id: string) {
+    return this.accountModel.findByIdAndDelete(id);
   }
 }
