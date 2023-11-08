1182a3144e927d66209cb763c0e1f94584e2f9d1
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-08 08:27:18 -0500

    feat(app.module): added account and group Module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GroupModule } from './group/group.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://localhost/administrator-db-api'),
+    GroupModule,
+    AccountModule,
   ],
   controllers: [AppController],
   providers: [AppService],
