4bdba28af56c702462b8dd558d298088d48d5a5b
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 08:58:36 -0500

    feat(team.module): added TeamRepository as a provider

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { TeamRepository } from './repository/mongo/team.repository.mongo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Team, TeamSchema } from './schemas/team.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{
+    name: Team.name,
+    schema: TeamSchema
+  }])],
   controllers: [TeamController],
-  providers: [TeamService],
+  providers: [TeamService, TeamRepository],
 })
 export class TeamModule {}
