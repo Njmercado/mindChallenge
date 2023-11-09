edfb68896428daf93a935c80295b447f31fe6430
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 08:47:39 -0500

    feat(team.schema): added name prop to team

--- src/team/schemas/team.schema.ts
+++ src/team/schemas/team.schema.ts
@@ -1,13 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { RegisterUser } from 'src/user/entities/register-user.entity';
+import { TeamUser } from 'src/user/entities/team-user.entity';
 
 export type TeamDocument = HydratedDocument<Team>;
 
 @Schema()
 export class Team {
   @Prop({ required: true })
-  users: RegisterUser[];
+  users: TeamUser[];
+
+  @Prop({required: true})
+  name: string;
 }
 
 export const TeamSchema = SchemaFactory.createForClass(Team);
