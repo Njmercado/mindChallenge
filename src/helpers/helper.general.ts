01166cf9ffcaeda62b711850c22edb6a2bc39867
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 13:48:20 -0500

    feat(helper.general): added func to cast strings to array of mongo id

--- src/helpers/helper.general.ts
+++ src/helpers/helper.general.ts
@@ -4,4 +4,8 @@ export class HelperGeneral {
   toMongoID(id: string) {
     return new mongoose.Types.ObjectId(id);
   }
+
+  arrayIdsToMongoIds(ids: string[]): mongoose.Types.ObjectId[] {
+    return ids.map(id => new mongoose.Types.ObjectId(id));
+  }
 }
