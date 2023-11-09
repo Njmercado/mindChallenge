a8d66e06d44ca3e7c2f9ea9884e4b9b4ff50ca48
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 13:49:15 -0500

    feat(team.schema): changed type of user array

--- src/team/schemas/team.schema.ts
+++ src/team/schemas/team.schema.ts
@@ -1,13 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
-import { TeamUser } from 'src/user/entities/team-user.entity';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type TeamDocument = HydratedDocument<Team>;
 
 @Schema()
 export class Team {
-  @Prop({ required: true })
-  users: TeamUser[];
+  @Prop({ required: true, ref: 'User' })
+  users: Array<mongoose.Types.ObjectId>; 
 
   @Prop({required: true})
   name: string;
