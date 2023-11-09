d09636f18217b08ae9d5066f0008f0c558b05a61
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 18:46:47 -0500

    feat(team.repository.mongo): added fucs to move, add and delete user from team

--- src/team/repository/mongo/team.repository.mongo.ts
+++ src/team/repository/mongo/team.repository.mongo.ts
@@ -7,30 +7,24 @@ import { CreateTeamDto } from "src/team/dto/create-team.dto";
 import { UpdateTeamDto } from "src/team/dto/update-team.dto";
 import { FilterTeamDto } from "src/team/dto/filter-team.dto";
 import { Injectable } from "@nestjs/common";
+import { User } from "src/user/schemas/user.schema";
 
 @Injectable()
 export class TeamRepository {
   constructor(
     @InjectModel(Team.name) private teamModel: Model<Team>,
+    @InjectModel(User.name) private userModel: Model<User>,
   ) {}
   helper = new HelperGeneral();
 
   async createTeam(createTeamDto: CreateTeamDto): Promise<void> {
     const { users, name } = createTeamDto;
     const usersAsMongoIds = this.helper.arrayIdsToMongoIds(users);
-    this.teamModel.create({users: usersAsMongoIds, name});
+
+    await this.teamModel.create({users: usersAsMongoIds, name});
+
+    this.updateUsersStatuses(users, [])
     // TODO: add event to team move
-  }
-
-  async addUsersToTeam(teamId: string, userIds: Array<string>): Promise<any> {
-    const team = await this.teamModel.findById(teamId).exec();
-    userIds.forEach(id => {
-      team.users.push(this.helper.toMongoID(id));
-    });
-
-    await team.save();
-
-    return team;
   }
 
   async findOneTeam(id: string) {
@@ -38,17 +32,128 @@ export class TeamRepository {
   }
 
   async updateTeam(teamId: string, updateTeamDto: UpdateTeamDto) {
-    //TODO: split userIds between added and deleted ones for events
-    const userIdsToMongoId = updateTeamDto.users.map(userId => this.helper.toMongoID(userId));
-    const update = {...updateTeamDto, userIdsToMongoId}
 
-    return await this.teamModel.findByIdAndUpdate(teamId, update);
+    const team = await this.teamModel.findById(teamId).exec();
+    const {users, name} = updateTeamDto;
+    //TODO: split userIds between added and deleted ones for events
+
+    if(users) {
+      const isUserIncluded = user => team.users.includes(
+        this.helper.toMongoID(user)
+      ); 
+      const usersToAdd = users.filter( user => !isUserIncluded(user));
+      const usersToDelete = team.users.filter(user => !users.includes(user.toString()));
+      
+      console.log([usersToDelete, usersToAdd]);
+
+      const responseDeletedUsers = usersToDelete.map(user => this.quitUserFromTeam(user.toString(), teamId));
+      const responseAddedUsers = usersToAdd.map(user => this.addUserToTeam(user, teamId));
+    }
+
+    if(name) {
+      team.name = name
+    }
+
+    await team.save();
+
+    return team;
   }
 
   async removeTeam(id: string) {
     // TODO: create an event to define when a user was removed from a team
     // TODO: create event for when a user were quit from the team
     return this.teamModel.findByIdAndRemove(id).exec()
+  }
+
+  async moveUser(userId: string, fromTeam: string, toTeam: string): Promise<{
+    message: string,
+    code: number,
+  }> {
+    const user = await this.userModel.findById(userId).exec();
+    const sourceTeam = await this.teamModel.findById(fromTeam).exec();
+    const targetTeam = await this.teamModel.findById(toTeam).exec();
+
+    if(user.hasAssignedTeam) {
+      return {
+        message: "Could not move user due this user has an assigned team",
+        code: 205 // TODO: change code
+      }
+    } else {
+      sourceTeam.users.splice(
+        sourceTeam.users.indexOf(this.helper.toMongoID(userId)),
+        1
+      )
+      // TODO: add event of quit user from team
+
+      targetTeam.users.push(this.helper.toMongoID(userId));
+      // TODO: add event of adding user to team
+
+      user.hasAssignedTeam = true;
+
+      sourceTeam.save();
+      targetTeam.save();
+      user.save();
+
+      return {
+        message: 'User moved successfully',
+        code: 200,
+      }
+    }
+  }
+
+  private async quitUserFromTeam(userId: string, teamId: string): Promise<{
+    message: string,
+    code: number
+  }> {
+    const team = await this.teamModel.findById(teamId).exec();
+
+    const userBelongsToTeam = () => team.users.includes(this.helper.toMongoID(userId));
+
+    if(userBelongsToTeam()) {
+      team.users.splice(
+        team.users.indexOf(this.helper.toMongoID(userId)),
+        1
+      )
+      
+      await team.save();
+
+      this.updateUserStatus(userId, false);
+
+      // TODO: add that user x has been deleted from team x into events
+
+      return {
+        message: `User ${userId} deleted from team ${team.name}`,
+        code: 200
+      }
+    } else {
+      return {
+        message: `User ${userId} does not exist in team ${team.name}`,
+        code: 404,
+      }
+    }
+  }
+
+  private async addUserToTeam(userId: string, teamId: string): Promise<{
+    message: string,
+    code: number
+  }> {
+    
+    const user = await this.userModel.findById(userId);
+    const userhasTeam = () => user.hasAssignedTeam;
+
+    if(userhasTeam()) {
+      return {
+        message: `User ${user.name} already assigned to team`,
+        code: 404,
+      }
+    } else {
+      const team = await this.teamModel.findById(teamId);
+      team.users.push(this.helper.toMongoID(userId));
+      user.hasAssignedTeam = true;
+
+      team.save();
+      user.save();
+    }
   }
 
   async filterTeam(filters: FilterTeamDto) {
@@ -66,5 +171,15 @@ export class TeamRepository {
     }).exec();
 
     return response;
+  }
+
+  private updateUsersStatuses(ids: string[], states: boolean[]) {
+    ids.forEach((id: string, index: number) => {
+      this.updateUserStatus(id, states[index]);
+    });
+  }
+
+  private updateUserStatus(id: string, state: boolean) {
+    this.userModel.findByIdAndUpdate(id, { $set: { hasAssignedTeam: state }}).exec();
   }
 }
