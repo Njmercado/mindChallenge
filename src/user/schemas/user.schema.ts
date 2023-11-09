4c891c520b652b1c03d06be510069eeb36c067c2
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 13:47:39 -0500

    feat(user.schema): changed email prop

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -8,8 +8,8 @@ export class User {
   @Prop({required: true})
   name: string;
 
-  @Prop({required: true})
-  mail: string;
+  @Prop({required: true, unique: true})
+  email: string;
 
   @Prop({required: true})
   password: string;
