1474514f5c4bcfe0294841230d9044e47fbfc4f1
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 18:45:44 -0500

    feat(create-team): added validators

--- src/team/dto/create-team.dto.ts
+++ src/team/dto/create-team.dto.ts
@@ -1,4 +1,11 @@
+import { IsArray, ArrayUnique, IsString, IsNotEmpty } from "class-validator";
+
 export class CreateTeamDto {
+  @IsArray()
+  @ArrayUnique()
   users: string[];
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
