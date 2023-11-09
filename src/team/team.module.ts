9af77aec42a6a7bb1e8d490262c291a547489052
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-09 18:46:08 -0500

    feat(team.module): added User and Team schema to module

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -4,13 +4,20 @@ import { TeamController } from './team.controller';
 import { TeamRepository } from './repository/mongo/team.repository.mongo';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Team, TeamSchema } from './schemas/team.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{
-      name: Team.name,
-      schema: TeamSchema
-    }]),
+    MongooseModule.forFeature([
+      {
+        name: Team.name,
+        schema: TeamSchema
+      },
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ]),
   ],
   controllers: [TeamController],
   providers: [TeamService, TeamRepository],
