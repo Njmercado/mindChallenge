c25f7d089270ab3568653858ae9031cc826726e6
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 10:45:52 -0500

    feat(user.module): export userService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -11,5 +11,6 @@ import { User, UserSchema } from './schemas/user.schema';
   }])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
