1a5c607d3ebca29c23f526cce51aa7c90d10d0f1
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 14:43:44 -0500

    feat(account.controller): added guards

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Role } from 'src/user/entities/role.enum';
+import { Roles } from 'src/auth/decorator/roles.decotaror';
 
+@ApiBearerAuth('access-token')
+@UseGuards(AuthGuard, RolesGuard)
+@Roles([Role.ADMIN, Role.SUPER_ADMIN])
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
