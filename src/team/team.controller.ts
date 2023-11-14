250b92261c5e976ceaf8e7c66ed6e3b5227c3408
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 06:58:03 -0500

    feat(team.controller): moved endpoint because it was generating error

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
@@ -13,11 +13,6 @@ export class TeamController {
     return this.teamService.create(createTeamDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.teamService.findOne(id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
     return this.teamService.update(id, updateTeamDto);
@@ -30,9 +25,14 @@ export class TeamController {
 
   @Get('/filter')
   filter(
-    @Body() filter: FilterTeamDto,
+    @Query() filter: FilterTeamDto,
   ) {
     return this.teamService.filter(filter);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.teamService.findOne(id);
   }
 
   @Put('/move/:userId/:fromTeam/:toTeam')
