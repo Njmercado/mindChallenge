b14c92cc2da182667dc5085259ef329f6aa3b619
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 10:49:27 -0500

    feat(auth.service): created service to auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService, 
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(email: string, password: string): Promise<any> {
+    const user = await this.userService.findOneByEmail(email);
+    if(!user.comparePassword(password)) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user._id.toString(), email: user.email, password: user.password };
+    const token = {
+      access_token: this.jwtService.sign(payload),
+    };
+
+    return token;
+  }
+}
