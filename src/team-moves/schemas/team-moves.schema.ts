ab56c257100cc435cc71f24dc090f0fa8fac9caa
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 03:15:50 -0500

    feat(team-moves.schema): added default values for few props

--- src/team-moves/schemas/team-moves.schema.ts
+++ src/team-moves/schemas/team-moves.schema.ts
@@ -1,10 +1,31 @@
-import { Prop, Schema } from "@nestjs/mongoose";
-import mongoose from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { TeamAction } from "../entities/team-action.enum";
+
+export type TeamMoveDocument = HydratedDocument<TeamMove>;
 
 @Schema()
 export class TeamMove {
   @Prop({ref: 'User'})
-  id: mongoose.Types.ObjectId;
-  addedAt: number = Date.now();
+  userId: mongoose.Types.ObjectId;
+  @Prop({ enum: TeamAction})
+  action: string;
+  @Prop({
+    type: Number,
+    default: function() {
+      return this.action === TeamAction.ADD && 
+        Date.now() 
+    } 
+  })
+  addedAt?: number;
+  @Prop({
+    type: Number,
+    default: function() {
+      return this.action === TeamAction.DELETE && 
+        Date.now()
+    } 
+  })
   outAt?: number;
 }
+
+export const TeamMoveSchema = SchemaFactory.createForClass(TeamMove);
