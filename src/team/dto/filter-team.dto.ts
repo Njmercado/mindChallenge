c38caf6a9626109deda7b5b7e9277341e7710c64
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 06:57:30 -0500

    feat(filter-team.dto): added decorator for documentation

--- src/team/dto/filter-team.dto.ts
+++ src/team/dto/filter-team.dto.ts
@@ -1,7 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { TeamAction } from "src/team-moves/entities/team-action.enum";
+
 export class FilterTeamDto {
-  id?: string;
-  name?: string;
-  userName?: string;
-  addedAt?: number;
-  outAt?: number;
+  @ApiProperty({ required: false })
+  id: string;
+
+  @ApiProperty({ required: false })
+  name: string;
+
+  @ApiProperty({ required: false })
+  userName: string;
+
+  @ApiProperty({ required: false, default: 0 })
+  startAt: number;
+
+  @ApiProperty({ required: false, default: Date.now()})
+  endAt: number;
+
+  @ApiProperty({ required: false, enum: TeamAction})
+  action: TeamAction;
 }
