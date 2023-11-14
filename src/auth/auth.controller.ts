e7b2494a48bb79d2d6f16574053f229b1adc8334
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-14 10:49:45 -0500

    feat(auth.controller): controller for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() login: LoginDto) {
+    return this.authService.signIn(login.email, login.password);
+  }
+}
