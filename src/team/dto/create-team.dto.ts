4327945ef6d138d246ff46e6ed93f1e7a03999ee
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-15 07:28:23 -0500

    feat(create-team): added api property

--- src/team/dto/create-team.dto.ts
+++ src/team/dto/create-team.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, ArrayUnique, IsString, IsNotEmpty } from "class-validator";
 
 export class CreateTeamDto {
   @IsArray()
   @ArrayUnique()
+  @ApiProperty({required: false})
   users: string[];
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({required: false})
   name: string;
 }
