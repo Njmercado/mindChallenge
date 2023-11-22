ee17ecc0e3c5d36ce96a4dbfa1f5c5cf0db644a1
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-22 13:04:01 -0500

    fix(controller): fixed roles error

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -10,31 +10,35 @@ import { Roles } from 'src/auth/decorator/roles.decotaror';
 
 @ApiBearerAuth('access-token')
 @UseGuards(AuthGuard, RolesGuard)
-@Roles([Role.ADMIN, Role.SUPER_ADMIN])
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Post()
   create(@Body() createAccountDto: CreateAccountDto) {
     return this.accountService.create(createAccountDto);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Get()
   findAll() {
     return this.accountService.findAll();
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.accountService.findOne(id);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAccountDto: UpdateAccountDto) {
     return this.accountService.update(id, updateAccountDto);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.accountService.remove(id);
--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,37 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query, UseGuards, Request } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/decorator/roles.decotaror';
-import { Role } from 'src/user/entities/role.enum';
+import { Role } from '../user/entities/role.enum';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { FilterTeamMovementsDto } from 'src/team-moves/dto/filter-team-movement.dto';
 import { FilterTeamDto } from './dto/filter-team.dto';
 
 @ApiBearerAuth('access-token')
 @UseGuards(AuthGuard, RolesGuard)
-@Roles([Role.ADMIN, Role.SUPER_ADMIN])
 @Controller('team')
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Post()
-  create(@Body() createTeamDto: CreateTeamDto) {
+  create(
+    @Body() createTeamDto: CreateTeamDto,
+    @Request() request: { user: any}
+  ) {
     return this.teamService.create(createTeamDto);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
     return this.teamService.update(id, updateTeamDto);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.teamService.remove(id);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Get('/filter')
   filter(
     @Query() filter: FilterTeamDto,
@@ -39,6 +45,7 @@ export class TeamController {
     return this.teamService.filter(filter);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Get('/filter-movements')
   filterTeamMovements(
     @Query() filter: FilterTeamMovementsDto,
@@ -46,11 +53,13 @@ export class TeamController {
     return this.teamService.filterTeamMovements(filter);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.teamService.findOne(id);
   }
 
+  @Roles([Role.ADMIN, Role.SUPER_ADMIN])
   @Put('/move/:userId/:fromTeam/:toTeam')
   move(
     @Param('userId') userId: string,
