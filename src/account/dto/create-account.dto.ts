890b633ba5177e573269cc7c5a38a5069ab3cb0b
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-22 13:03:35 -0500

    feat(dto): added api property

--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateAccountDto {
+  @ApiProperty({required: false})
   name: string;
+
+  @ApiProperty({required: false})
   clientName: string;
+
   //This is the responsable, user, id.
+  @ApiProperty({required: false})
   responsable: string;
 }
--- src/team/dto/create-team.dto.ts
+++ src/team/dto/create-team.dto.ts
@@ -1,14 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsArray, ArrayUnique, IsString, IsNotEmpty } from "class-validator";
+import { IsArray, ArrayUnique, IsString, IsNotEmpty, IsOptional } from "class-validator";
 
 export class CreateTeamDto {
   @IsArray()
   @ArrayUnique()
   @ApiProperty({required: false})
+  @IsOptional()
   users: string[];
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({required: false})
+  @ApiProperty({required: true})
   name: string;
 }
