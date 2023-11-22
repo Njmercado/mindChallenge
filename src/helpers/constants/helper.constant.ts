a8cb961a74ae5277609c1241ee69b9869aadbfdc
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-22 13:04:51 -0500

    feat(helper.contant): added constants for tests

---
+++ src/helpers/constants/helper.constant.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+export const IDs = [
+  "5e48197979a7570000000000",
+  "5e48197979a7570000000001",
+  "5e48197979a7570000000002",
+]
+
+export const IDsToAdd = [
+  "5e48197979a7570000000003",
+]
+
+export const IDsToAddAsObjectIds = [
+  new mongoose.Types.ObjectId("5e48197979a7570000000003"),
+]
+
+export const IDsAsObjectIds = [
+  new mongoose.Types.ObjectId("5e48197979a7570000000000"),
+  new mongoose.Types.ObjectId("5e48197979a7570000000001"),
+  new mongoose.Types.ObjectId("5e48197979a7570000000002"),
+]
