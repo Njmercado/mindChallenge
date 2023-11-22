a92ec13d6061f7e69bbcfd824f1e0cdb82449e65
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-22 13:05:29 -0500

    feat(team.repository): fixed few errors and refactor filters

--- src/team/repository/mongo/team.repository.mongo.ts
+++ src/team/repository/mongo/team.repository.mongo.ts
@@ -1,29 +1,48 @@
 import { InjectModel } from "@nestjs/mongoose";
 import { Team, TeamDocument } from "../../schemas/team.schema";
 import { Model } from "mongoose";
-import { HelperGeneral } from "src/helpers/helper.general";
+import { HelperGeneral } from "../../../helpers/helper.general";
 import { CreateTeamDto } from "src/team/dto/create-team.dto";
 import { UpdateTeamDto } from "src/team/dto/update-team.dto";
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { User } from "src/user/schemas/user.schema";
+import { User } from "../../../user/schemas/user.schema";
 import { FilterTeamMovementsDto } from "src/team-moves/dto/filter-team-movement.dto";
 import { FilterTeamDto } from "src/team/dto/filter-team.dto";
+import { ExceptionsHandler } from "@nestjs/core/exceptions/exceptions-handler";
+import { TeamMove } from "src/team-moves/schemas/team-moves.schema";
 
 @Injectable()
 export class TeamRepository {
   constructor(
     @InjectModel(Team.name) private teamModel: Model<Team>,
     @InjectModel(User.name) private userModel: Model<User>,
-  ) {}
+    @InjectModel(TeamMove.name) private teamMoveModel: Model<TeamMove>,
+  ) { }
   helper = new HelperGeneral();
 
-  async createTeam(createTeamDto: CreateTeamDto): Promise<void> {
-    const { users, name } = createTeamDto;
-    const usersAsMongoIds = this.helper.arrayIdsToMongoIds(users);
+  async createTeam(createTeamDto: CreateTeamDto): Promise<any> {
+    try {
+      const { users, name } = createTeamDto;
+      const usersAsMongoIds = this.helper.arrayIdsToMongoIds(users);
 
-    await this.teamModel.create({users: usersAsMongoIds, name});
+      const team = await this.teamModel.create({ users: usersAsMongoIds, name });
 
-    this.updateUsersStatuses(users, [])
+      if (team) {
+        this.updateUsersStatuses(users, [])
+
+        return {
+          message: 'Team created successfully',
+          code: HttpStatus.OK
+        }
+      } else {
+        return {
+          message: 'Team could not be created',
+          code: HttpStatus.EXPECTATION_FAILED
+        }
+      }
+    } catch (err) {
+      throw new ExceptionsHandler(err);
+    }
   }
 
   async findOneTeam(id: string) {
@@ -36,12 +55,14 @@ export class TeamRepository {
   }> {
 
     const team = await this.teamModel.findById(teamId).exec();
-    const {users, name} = updateTeamDto;
-    if(users) {
+    const { users, name } = updateTeamDto;
+    if (users) {
+      const currentUsersAsString = this.helper.arrayMongoIdsToString(team.users);
       const usersToAdd = users.filter(
-        user => !team.users.includes(
-          this.helper.toMongoID(user)
-        )
+        user =>
+          !currentUsersAsString.includes(
+            user
+          )
       );
       const usersToDelete = team.users.filter(
         user => !users.includes(
@@ -49,16 +70,16 @@ export class TeamRepository {
         )
       );
 
-      for(const user of usersToDelete) {
+      for (const user of usersToDelete) {
         await this.deleteUserFromTeam(user.toString(), team);
       }
 
-      for(const user of usersToAdd) {
+      for (const user of usersToAdd) {
         await this.addUserToTeam(user, team)
       }
     }
 
-    if(name) {
+    if (name) {
       team.name = name
     }
 
@@ -66,7 +87,7 @@ export class TeamRepository {
 
     return {
       message: `Team ${team.name} udpated successfully`,
-      code: 200,
+      code: HttpStatus.OK,
     };
   }
 
@@ -78,9 +99,9 @@ export class TeamRepository {
       }
     ).exec()
 
-    if(response) {
+    if (response) {
       const team = await this.teamModel.findById(id).exec();
-      for(const user of team.users) {
+      for (const user of team.users) {
         await this.deleteUserFromTeam(user.toString(), team);
       }
     }
@@ -94,38 +115,39 @@ export class TeamRepository {
     const sourceTeam = await this.teamModel.findById(fromTeam).exec();
     const targetTeam = await this.teamModel.findById(toTeam).exec();
 
-    if(user.hasAssignedTeam) {
+    if (user.hasAssignedTeam) {
       return {
         message: "Could not move user due this user has an assigned team",
-        code: 205 // TODO: change code
+        code: HttpStatus.CONFLICT
       }
     } else {
+      const userIdToObjectId = this.helper.toMongoID(userId);
       sourceTeam.users.splice(
-        sourceTeam.users.indexOf(this.helper.toMongoID(userId)),
+        sourceTeam.users.indexOf(userIdToObjectId),
         1
       )
-      targetTeam.users.push(this.helper.toMongoID(userId));
+      targetTeam.users.push(userIdToObjectId);
       user.hasAssignedTeam = true;
 
-      sourceTeam.save();
-      targetTeam.save();
+      await sourceTeam.save();
+      await targetTeam.save();
       await user.save();
 
       return {
         message: 'User moved successfully',
-        code: 200,
+        code: HttpStatus.OK,
       }
     }
   }
 
-  private async deleteUserFromTeam(userId: string, team: TeamDocument): Promise<{
+  async deleteUserFromTeam(userId: string, team: TeamDocument): Promise<{
     message: string,
     code: number,
     data: any,
   }> {
     const userBelongsToTeam = () => team.users.includes(this.helper.toMongoID(userId));
 
-    if(userBelongsToTeam()) {
+    if (userBelongsToTeam()) {
       team.users.splice(
         team.users.indexOf(this.helper.toMongoID(userId)),
         1
@@ -147,15 +169,15 @@ export class TeamRepository {
     }
   }
 
-  private async addUserToTeam(userId: string, team: TeamDocument): Promise<{
+  async addUserToTeam(userId: string, team: TeamDocument): Promise<{
     message: string,
     code: number
   }> {
-    
+
     const user = await this.userModel.findById(userId);
     const userhasTeam = () => user.hasAssignedTeam;
 
-    if(userhasTeam()) {
+    if (userhasTeam()) {
       return {
         message: `User ${user.name} already assigned to team`,
         code: 404,
@@ -171,52 +193,69 @@ export class TeamRepository {
   // TODO: Add pagination
   async filterTeamMovements(filters: FilterTeamMovementsDto) {
     try {
-      return await this.teamModel.find({})
-      .populate({
-        path: 'moves.userId',
-        select: 'name email',
-        match: {
-          ...(filters.nameOfTheUser && { name: { $regex: filters.nameOfTheUser, $options: 'i' }}),
-          ...(filters.userEmail && { email: { $regex: filters.userEmail, $options: 'i' }}),
-        }
+      return this.teamMoveModel.find({
+        ...(filters.action && { action: filters.action }),
+        ...(filters.startAt && { addedAt: { $gte: filters.startAt } }),
+        ...(filters.endAt && { outAt: { $lte: filters.endAt } }),
       })
-      .select('name moves.action moves.addedAt moves.outAt')
-      .exec()
-      .then(function(results) {
-        return results.map( result => {
-          return {
-            name: result.name,
-            _id: result._id,
-            moves: result.moves.filter( move => 
-              move.userId !== null &&
-              (filters.action ? move.action === filters.action: true) &&
-              move.addedAt >= Number(filters.startAt) &&
-              move.outAt <= Number(filters.endAt)
-            ) 
+        .populate({
+          path: 'userId',
+          select: '-_id name email',
+          match: {
+            ...(
+              filters.nameOfTheUser &&
+              {
+                name: {
+                  $regex: filters.nameOfTheUser,
+                  $options: 'i'
+                }
+              }),
+            ...(
+              filters.userEmail &&
+              {
+                email: {
+                  $regex: filters.userEmail,
+                  $options: 'i'
+                }
+              })
           }
         })
-      })
+        .populate({
+          path: 'team',
+          select: '-_id name'
+        })
+        .exec()
+        .then(function (results) {
+          return results.filter(result => result.userId !== null);
+        });
     } catch (error) {
+      console.log(error);
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async filter(filters: FilterTeamDto) {
     try {
-      return await this.teamModel.find({
-        ...(filters.name && { name: { $regex: filters.name, $options: 'i' }}),
+      return this.teamModel.find({
+        ...(filters.name && { name: { $regex: filters.name, $options: 'i' } }),
       })
-      .populate({
-        path: 'users',
-        match: {
-          ...(filters.userName && { name: { $regex: filters.userName, $options: 'i' } }),
-          ...(filters.userEmail && { email: {$regex: filters.userEmail, $options: 'i' }}),
-        },
-        select: 'name email role'
-      })
-      .select('users name')
-      .exec();
-    } catch(error) {
+        .populate({
+          path: 'users',
+          match: {
+            ...(filters.userName && { name: { $regex: filters.userName, $options: 'i' } }),
+            ...(filters.userEmail && { email: { $regex: filters.userEmail, $options: 'i' } }),
+          },
+          select: 'name email role'
+        })
+        .select('users name')
+        .exec()
+        .then(function (results) {
+          return results.filter(result => result.users.length > 0 && {
+            users: result.users.filter(user => user !== null),
+            name: result.name
+          })
+        })
+    } catch (error) {
       console.error(error)
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -229,7 +268,7 @@ export class TeamRepository {
   }
 
   // TODO: Move this to user service
-  private updateUserStatus(id: string, state: boolean) {
-    this.userModel.findByIdAndUpdate(id, { $set: { hasAssignedTeam: state }}).exec();
+  async updateUserStatus(id: string, state: boolean) {
+    await this.userModel.findByIdAndUpdate(id, { $set: { hasAssignedTeam: state } }).exec();
   }
 }
