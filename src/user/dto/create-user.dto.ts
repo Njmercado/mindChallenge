20825bb31dd8e64bab58892d2dc8dedc38242dd6
Nino Mercado <njmercado@uninorte.edu.co>
2023-11-22 14:40:06 -0500

    fix(user-dto): fixed user dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Role } from "../entities/role.enum";
-import { IsEmail, IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsEnum, IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({required: true})
@@ -14,15 +14,14 @@ export class CreateUserDto {
   @IsEmail()
   email: string;
 
-  // TODO: add validator for password
   @ApiProperty({required: true})
   @IsNotEmpty()
   @MinLength(8)
-  @MaxLength(100)
+  @MaxLength(16)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'invalid password, remember password must have number, upppercase letters, lowercase letters, simbols and be at leat 8 characters long'})
   password: string;
 
-  @ApiProperty({required: true, enum: Role, default: Role.USER})
-  @IsNotEmpty()
-  role: Role;
+  @ApiProperty({required: true})
+  @IsEnum(Role)
+  role: Role = Role.USER;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsEmpty, IsEnum, IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../entities/role.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty({required: false})
+  @MinLength(10)
+  @MaxLength(100)
+  name?: string;
+
+  @ApiProperty({required: false})
+  @IsEnum(Role)
+  role?: Role = Role.USER;
+}
